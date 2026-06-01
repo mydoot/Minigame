987f21d4fd6f582f48de3da5cfc6aa69adea7ba4
mydoot <buaqur@gmail.com>
2026-05-31 23:15:26 -0700

    work on the track handler

--- Assets/Scripts/TrackHandler.cs
+++ Assets/Scripts/TrackHandler.cs
@@ -1,16 +1,45 @@
+using System;
 using UnityEngine;
+
+// Handles spawning in the notes and moving them betwen the spawn point and the hit point
 
 public class TrackHandler : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    [SerializeField] private GameObject note;
+    [SerializeField] private Transform hitPoint;
+    [SerializeField] private Transform spawnPoint;
+
+    [SerializeField] private ConductorScript conductor;
+
+    // beat info
+    private float spawnBeat;
+    private float targetBeat = 1f; // the beat where the note should hit the line; should be a list but for testing it will be 1 value
+    private float currentBeat;
+    [SerializeField] private float shownBeats = 4f;
+
+    // might have to create a list/dictionary of note types in the future depending if we want to have multiple note types
+
     void Start()
     {
-        
+        spawnBeat = targetBeat - shownBeats;
     }
 
-    // Update is called once per frame
+    /* 
+        TODO
+        - Create a list/other method to contain the "chart" of the track
+        - create a function that recalculates the target beat
+        - solidify designs
+    
+    */
     void Update()
     {
+        currentBeat = conductor.songPositionInBeats;
         
+        transform.position = Vector2.Lerp(
+        spawnPoint.position,
+        hitPoint.position,
+        (currentBeat - spawnBeat) / (targetBeat - spawnBeat)
+    );
     }
 }
